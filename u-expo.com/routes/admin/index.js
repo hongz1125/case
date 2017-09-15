2f938b3ffea6bb7967dc2a8345999f99f8c644e2
zhihong <hongz1125@qq.com>
2017-09-15 16:39:22 +0800

    hz up

--- u-expo.com/routes/admin/index.js
+++ u-expo.com/routes/admin/index.js
@@ -8,35 +8,39 @@ router.get('/', function(req, res, next) {
     res.redirect('/login');
     return;
   }
+  param.nav = '0';
   res.render('admin/index.html', param);
 });
 
-//进入 项目 编辑页 
-router.get('/edit', function(req, res, next) {
+//项目 列表页
+router.get('/case', function(req, res, next) {
   let param = {};
   if (!req.cookies.is_login){
     res.redirect('/login');
     return;
   }
-  res.render('admin/index_edit.html', param);
+  param.nav = 'case';
+  res.render('admin/case.html', param);
 });
-
-
-
-router.get('/edit', function(req, res, next) {
+//项目 编辑页 
+router.get('/case/edit', function(req, res, next) {
   let param = {};
   if (!req.cookies.is_login){
     res.redirect('/login');
     return;
   }
-  res.render('admin/index_edit.html', param);
+  param.nav = 'case';
+  res.render('admin/case_edit.html', param);
 });
+
+//标签 列表页
 router.get('/tag', function(req, res, next) {
   let param = {};
   if (!req.cookies.is_login){
     res.redirect('/login');
     return;
   }
+  param.nav = 'tag';
   res.render('admin/tag.html', param);
 });
 
