61a133685b58f114c3d7bc7d236437be8eece713
root <root@u-expo.(none)>
2017-11-15 14:02:27 +0800

    hz up

--- admin.u-expo.com/config/sql.js
+++ admin.u-expo.com/config/sql.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool  = mysql.createPool({
   connectionLimit : 10,
-  host : '121.40.37.200',
+  host : '39.106.121.93',
   user : 'root',
   password : 'Hongz1126',
-  database : 'u-expo'
+  database : 'expo'
 });
 
 module.exports = pool;
